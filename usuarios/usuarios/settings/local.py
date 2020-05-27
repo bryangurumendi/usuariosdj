6e5d674c9c3ea56f7997d247a63d4aabaad7d74e
bryan <bryangurumendi@hotmail.com>
2020-05-26 19:06:50 -0500

    configuracion del proyecto

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,29 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost']
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret_key('DB_NAME'),
+        'USER': get_secret_key('USER'),
+        'PASSWORD': get_secret_key('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
